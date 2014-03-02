437d7a972c1ea05973d256b2d6eb01ac539ea2f6
Ben Jovanic <ben.jovanic.11@aberdeen.ac.uk>
2014-03-02 12:25:37 +0000

    altered db.php to always connect to remote DB when running on heroku

--- inc/db.php
+++ inc/db.php
@@ -1,6 +1,11 @@
 <?php
-// $mysqli = new mysqli('176.32.230.14', 'web14-appoint', 'nomorejsp', 'web14-appoint');
-$mysqli = new mysqli('localhost', 'root', 'password', 'web14-appoint');
+if (preg_match("/(herokuapp)/i", $_SERVER['HTTP_HOST']) {
+	$mysqli = new mysqli('176.32.230.14', 'web14-appoint', 'nomorejsp', 'web14-appoint');
+}
+else {
+	// $mysqli = new mysqli('176.32.230.14', 'web14-appoint', 'nomorejsp', 'web14-appoint');
+	$mysqli = new mysqli('localhost', 'root', 'password', 'web14-appoint');
+}
 
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
